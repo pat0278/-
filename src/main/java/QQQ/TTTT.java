36835f39f869b89cc5ed0c0312ecefedf28e94dc
ahxuan <xuan45678901@gmail.com>
2024-10-23 15:46:24 +0800

    UP

---
+++ src/main/java/QQQ/TTTT.java
@@ -0,0 +1,14 @@
+package QQQ;
+
+public class TTTT {
+
+	public static void main(String[] args) {
+		int i, j;
+		for (i = 1; i <= 9; i++) {
+			for (j = 1; j <= 9; j++)
+				System.out.print(i + "*" + j + "=" + i * j + "\t");
+			System.out.println();
+		}
+	}
+
+}
