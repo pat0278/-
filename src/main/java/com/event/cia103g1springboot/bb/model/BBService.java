583a0b461f4b32a1e0a0e04a734831317aa82d32
Feng <mm950490@gmail.com>
2024-12-11 14:49:10 +0800

    測試

---
+++ src/main/java/com/event/cia103g1springboot/bb/model/BBService.java
@@ -0,0 +1,56 @@
+
+
+package com.event.cia103g1springboot.bb.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import hibernate.util.CompositeQuery.HibernateUtil_CompositeQuery_BB;
+
+@Service("bbService")
+public class BBService{
+
+	@Autowired
+	BBRepository repository;
+
+	
+	public void addMsg(BBVO bbVO) {
+		repository.save(bbVO);
+	}
+	
+	public void updateMsg(BBVO bbVO) {
+		repository.save(bbVO);
+	}
+	
+	public void deleteMsg(Integer msgid) {
+		if(repository.existsById(msgid)){
+			repository.deleteByMsgId(msgid);
+		}
+	}
+	
+	public BBVO getOneMsg(Integer msgid) {
+		Optional<BBVO> optional = repository.findById(msgid);
+		return optional.orElse(null);
+	}
+	
+	public List<BBVO> getAll(){
+		return repository.getAllMsg();
+	}
+
+//	要自己打開~~
+//	public List<BBVO> getAll(Map<String, String[]> map){
+//		return HibernateUtil_CompositeQuery_BB.getAllC(map,sessionFactory.openSession());
+//	}
+	
+	public List<BBVO> getPostMsg(){
+		return repository.getPostMsg();
+	}
+	
+	
+	
+}
