59b0a18479afdcf37cd76442e91b579595b3887f
WenFang <rfang1221@gmail.com>
2024-12-12 17:44:46 +0800

    新增綠界

---
+++ src/main/java/com/event/cia103g1springboot/com/example/ECPayDemo/OrderController.java
@@ -0,0 +1,20 @@
+package com.event.cia103g1springboot.com.example.ECPayDemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class OrderController {
+
+	@Autowired
+	OrderService orderService;
+
+	@PostMapping("/ecpayCheckout")
+	public String ecpayCheckout() {
+		String aioCheckOutALLForm = orderService.ecpayCheckout();
+
+		return aioCheckOutALLForm;
+	}
+}
